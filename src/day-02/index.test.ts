bea5b5207dad113f0a91244ed21e31d8a48cdd8a
Macklin Underdown <macklinu@users.noreply.github.com>
2017-12-06 15:08:29 -0500

    Solve Day 2 Part 2

--- src/day-02/index.test.ts
+++ src/day-02/index.test.ts
@@ -2,14 +2,39 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as util from 'util'
 
-import { calculateTotalSquareFeet } from './'
+import { createCalculator } from './'
 
 test('solves part 1', async () => {
+  let calculateTotalSquareFeet = createCalculator((total, [l, w, h]) => {
+    let lw = l * w
+    let wh = w * h
+    let hl = h * l
+    let shortestSide = Math.min.apply(Math, [lw, wh, hl])
+    return total + (2 * lw + 2 * wh + 2 * hl + shortestSide)
+  })
   expect(calculateTotalSquareFeet('2x3x4')).toBe(58)
   expect(calculateTotalSquareFeet('1x1x10')).toBe(43)
 
   let puzzleInput = await getPuzzleInput()
   expect(calculateTotalSquareFeet(puzzleInput)).toBe(1598415)
+})
+
+test('solves part 2', async () => {
+  let calculateTotalRibbon = createCalculator((total, dimensions) => {
+    // sort dimensions from least to greatest
+    dimensions.sort((a, b) => a - b)
+
+    let perimeter = 2 * (dimensions[0] + dimensions[1])
+    let volume = dimensions.reduce((acc, next) => acc * next)
+
+    return total + (perimeter + volume)
+  })
+
+  expect(calculateTotalRibbon('2x3x4')).toBe(34)
+  expect(calculateTotalRibbon('1x1x10')).toBe(14)
+
+  let puzzleInput = await getPuzzleInput()
+  expect(calculateTotalRibbon(puzzleInput)).toBe(3812909)
 })
 
 async function getPuzzleInput(): Promise<string> {
--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -1,13 +1,11 @@
-export function calculateTotalSquareFeet(input: string): number {
-  return input
-    .split('\n')
-    .filter(Boolean)
-    .map(str => str.split('x').map(Number))
-    .reduce((total, [l, w, h]) => {
-      let lw = l * w
-      let wh = w * h
-      let hl = h * l
-      let shortestSide = Math.min.apply(Math, [lw, wh, hl])
-      return total + (2 * lw + 2 * wh + 2 * hl + shortestSide)
-    }, 0)
+export function createCalculator(
+  reducer: (total: number, dimensions: number[]) => number
+) {
+  return function(input: string): number {
+    return input
+      .split('\n')
+      .filter(Boolean)
+      .map(str => str.split('x').map(Number))
+      .reduce(reducer, 0)
+  }
 }
