3005891160de2f829b6ea98fbcafbb3371e6e4a9
Macklin Underdown <macklinu@users.noreply.github.com>
2017-12-06 14:01:44 -0500

    Solve Day 1 Part 1

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,1 +1,6 @@
-// TODO
+export function determineFloorFromInstructions(input: string): number {
+  return input
+    .split('')
+    .filter(Boolean)
+    .reduce((num, str) => (str === '(' ? num + 1 : num - 1), 0)
+}
