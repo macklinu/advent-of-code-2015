5b7aa84ec1f9ffe127fbe55b01897a3e6ed3754c
Macklin Underdown <macklinu@users.noreply.github.com>
2017-12-06 14:24:15 -0500

    Solve Day 1 Part 2

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,6 +1,24 @@
 export function determineFloorFromInstructions(input: string): number {
-  return input
-    .split('')
-    .filter(Boolean)
-    .reduce((num, str) => (str === '(' ? num + 1 : num - 1), 0)
+  return toArray(input).reduce(
+    (floor, instruction) => (instruction === '(' ? floor + 1 : floor - 1),
+    0
+  )
+}
+
+export function determinePositionOfEnteringBasement(input: string): number {
+  let instructions = toArray(input)
+  let floor = 0
+
+  for (let index = 0; index < instructions.length; index++) {
+    let instruction = instructions[index]
+    floor = instruction === '(' ? floor + 1 : floor - 1
+    if (floor === -1) {
+      return index + 1
+    }
+  }
+  return -1
+}
+
+function toArray(input: string): string[] {
+  return input.split('').filter(Boolean)
 }
