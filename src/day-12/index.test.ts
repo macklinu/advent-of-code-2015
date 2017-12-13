9da3dc56cd255601e2fcf61a0d7dd206e0beba34
Macklin Underdown <macklinu@users.noreply.github.com>
2017-12-12 20:02:07 -0500

    Solve Day 12 Part 2

--- src/day-12/index.test.ts
+++ src/day-12/index.test.ts
@@ -1,6 +1,10 @@
 import puzzleInput from './puzzleInput'
-import { findSumOfAllNumbers } from './index'
+import { findSumOfAllNumbers, findSumOfAllNumbersIgnoringRed } from './index'
 
 test('solves part 1', () => {
-	expect(findSumOfAllNumbers(puzzleInput)).toBe(156366)
+  expect(findSumOfAllNumbers(puzzleInput)).toBe(156366)
+})
+
+test('solves part 2', () => {
+  expect(findSumOfAllNumbersIgnoringRed(puzzleInput)).toBe(96852)
 })
--- src/day-12/index.ts
+++ src/day-12/index.ts
@@ -1,4 +1,25 @@
+import is from '@sindresorhus/is'
+
 export function findSumOfAllNumbers(input: string): number {
   let match = input.match(/-?[0-9]+/g) || []
   return match.reduce((sum, m) => sum + Number(m), 0)
+}
+
+export function findSumOfAllNumbersIgnoringRed(input: string): number {
+  function traverseNode(node: any): number {
+    if (is.number(node)) {
+      return node
+    }
+    if (is.array(node)) {
+      return node.reduce((sum, n) => sum + traverseNode(n), 0)
+    }
+    if (is.object(node)) {
+      let values = Object.values(node)
+      return values.includes('red') ? 0 : traverseNode(values)
+    }
+    return 0
+  }
+
+  let json = JSON.parse(input)
+  return traverseNode(json)
 }
