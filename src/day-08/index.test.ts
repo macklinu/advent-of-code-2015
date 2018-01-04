399170339b6236fb6dc2813b56c5ef965d12489d
Macklin Underdown <macklinu@users.noreply.github.com>
2018-01-04 14:04:41 -0500

    Solve Day 8 Part 2

--- src/day-08/index.test.ts
+++ src/day-08/index.test.ts
@@ -2,14 +2,22 @@ import * as path from 'path'
 import * as fs from 'fs'
 import * as util from 'util'
 
-import { solvePuzzle } from './'
+import { solvePartOne, solvePartTwo } from './'
 
 test('solves part 1', async () => {
   let exampleInput = await readFile('example-input.txt')
-  expect(solvePuzzle(exampleInput)).toBe(12)
+  expect(solvePartOne(exampleInput)).toBe(12)
 
   let puzzleInput = await readFile('puzzle-input.txt')
-  expect(solvePuzzle(puzzleInput)).toBe(1371)
+  expect(solvePartOne(puzzleInput)).toBe(1371)
+})
+
+test('solves part 2', async () => {
+  let exampleInput = await readFile('example-input.txt')
+  expect(solvePartTwo(exampleInput)).toBe(19)
+
+  let puzzleInput = await readFile('puzzle-input.txt')
+  expect(solvePartTwo(puzzleInput)).toBe(2117)
 })
 
 async function readFile(filename: string): Promise<string> {
--- src/day-08/index.ts
+++ src/day-08/index.ts
@@ -1,6 +1,13 @@
-export function solvePuzzle(input: string): number {
-  let strings = input.split('\n').filter(Boolean)
-  return strings.reduce((sum, str) => {
+export function solvePartOne(input: string): number {
+  return parseInput(input).reduce((sum, str) => {
     return sum + (str.length - eval(str).length)
   }, 0)
 }
+
+export function solvePartTwo(input: string): number {
+  return parseInput(input).reduce((sum, str) => {
+    return sum + (JSON.stringify(str).length - str.length)
+  }, 0)
+}
+
+let parseInput = (input: string): string[] => input.split('\n').filter(Boolean)
